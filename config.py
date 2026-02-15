b6398f8a06f318d1d80bc6c16d9d647adef3575c
Hugo <weldu@yandex.com>
2026-02-15 16:35:34 +1300

    simplify site description

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ SITE_CONFIG = {
     "name": "Agni Parthene",
     "title": "Ἁγνὴ Παρθένε - Agni Parthene",
     "url": "https://agniparthene.nz",
-    "description": "The Agni Parthene (O Virgin Pure) hymn by St. Nectarios of Aegina, with Greek and English text side-by-side for Orthodox chanters.",
+    "description": "The Agni Parthene (O Virgin Pure) hymn by St. Nectarios of Aegina, with Greek and English text side-by-side.",
     "author": "St. Nectarios of Aegina",
     "language": "en",
     "locale": "en_NZ",
